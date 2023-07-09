8dfe019a114aa50a98835091ce23a2b99c9f21aa
Andrii-Skr <andrii.skrypchenko@gmail.com>
2023-07-09 16:11:20 +0300

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import checkerPlugin from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/quizzes/",
   plugins: [
     react(),
     checkerPlugin({
